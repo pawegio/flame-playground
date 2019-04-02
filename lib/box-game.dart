768e29c088129cc119be3a21ffc02d188adabc53
Paweł Gajda <pawegio@gmail.com>
2019-04-02 22:55:42 +0200

    Render rectangles

--- lib/box-game.dart
+++ lib/box-game.dart
@@ -3,13 +3,33 @@ import 'dart:ui';
 import 'package:flame/game.dart';
 
 class BoxGame extends Game {
+  Size screenSize;
+
   @override
   void render(Canvas canvas) {
-    // TODO: implement render
+    // draw black background
+    Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
+    Paint bgPaint = Paint();
+    bgPaint.color = Color(0xff000000);
+    canvas.drawRect(bgRect, bgPaint);
+
+    // draw white box
+    double screenCenterX = screenSize.width / 2;
+    double screenCenterY = screenSize.height / 2;
+    Rect boxRect = Rect.fromLTWH(screenCenterX - 75, screenCenterY - 75, 150, 150);
+    Paint boxPaint = Paint();
+    boxPaint.color = Color(0xffffffff);
+    canvas.drawRect(boxRect, boxPaint);
   }
 
   @override
   void update(double t) {
     // TODO: implement update
+  }
+
+  @override
+  void resize(Size size) {
+    screenSize = size;
+    super.resize(size);
   }
 }
