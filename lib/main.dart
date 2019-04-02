d40fc3246ac75a23fb1e319e55db917537a02651
Paweł Gajda <pawegio@gmail.com>
2019-04-02 22:39:23 +0200

    Add box game

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,13 @@
+import 'package:boxgame/box-game.dart';
+import 'package:flame/util.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() async {
+  Util flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  BoxGame game = BoxGame();
+  runApp(game.widget);
+}
